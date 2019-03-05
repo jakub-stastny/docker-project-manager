5e514c4d09867e9031e72b54494acb2b00b2a8e5
Jakub Šťastný <jakub.stastny.pt+git@gmail.com>
2019-03-05 22:05:12 +0000

    Comment out specs to see if Travis keeps failing

--- spec/unit/commands/init_spec.cr
+++ spec/unit/commands/init_spec.cr
@@ -1,33 +1,33 @@
-require "command"
-require "commands/init"
-require "spec_helper"
+# require "command"
+# require "commands/init"
+# require "spec_helper"
 
-describe DockerProjectManager::Init do
-  describe "#run" do
-    # it "requires an argument" do
-    #   expect_raises(DockerProjectManager::NoCommandError) do
-    #     DockerProjectManager::Command.run(["init"])
-    #   end
-    # end
+# describe DockerProjectManager::Init do
+#   describe "#run" do
+#     # it "requires an argument" do
+#     #   expect_raises(DockerProjectManager::NoCommandError) do
+#     #     DockerProjectManager::Command.run(["init"])
+#     #   end
+#     # end
 
-    # TODO: Move this to E2E tests.
-    # This should only test #validate, #usage etc.
-    # The #run method needs to be tested in a more holistic manner.
-    pending "creates a directory matching the project name" do
-      command = DockerProjectManager::Init.new("init", ["my-blog", "../../templates"])
-      tmp do
-        command.run
+#     # TODO: Move this to E2E tests.
+#     # This should only test #validate, #usage etc.
+#     # The #run method needs to be tested in a more holistic manner.
+#     pending "creates a directory matching the project name" do
+#       command = DockerProjectManager::Init.new("init", ["my-blog", "../../templates"])
+#       tmp do
+#         command.run
 
-        Dir.exists?("my-blog").should be_true
-        Dir.cd "my-blog"
-        File.exists?("README.md").should be_true
-        File.exists?("Dockerfile").should be_true
-        File.read("README.md").should_not match(/project_name/)
+#         Dir.exists?("my-blog").should be_true
+#         Dir.cd "my-blog"
+#         File.exists?("README.md").should be_true
+#         File.exists?("Dockerfile").should be_true
+#         File.read("README.md").should_not match(/project_name/)
 
-        Dir.exists?(".ssh").should be_true
-        File.exists?(".ssh/id_rsa").should be_true
-        File.exists?(".ssh/id_rsa.pub").should be_true
-      end
-    end
-  end
-end
+#         Dir.exists?(".ssh").should be_true
+#         File.exists?(".ssh/id_rsa").should be_true
+#         File.exists?(".ssh/id_rsa.pub").should be_true
+#       end
+#     end
+#   end
+# end
--- spec/unit/spec_helper.cr
+++ spec/unit/spec_helper.cr
@@ -1,9 +1,9 @@
-require "spec"
-require "file_utils"
+# require "spec"
+# require "file_utils"
 
-def tmp(&block)
-  Dir.mkdir("tmp") unless Dir.exists?("tmp")
-  Dir.cd("tmp", &block)
-ensure
-  FileUtils.rm_r("tmp")
-end
+# def tmp(&block)
+#   Dir.mkdir("tmp") unless Dir.exists?("tmp")
+#   Dir.cd("tmp", &block)
+# ensure
+#   FileUtils.rm_r("tmp")
+# end
